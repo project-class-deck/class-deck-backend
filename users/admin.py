6c510a45265267a3f99722476d38e192067a5700
hoetaek <hoetaek@student.snue.ac.kr>
2024-04-13 16:07:08 +0900

    django admin for school

--- users/admin.py
+++ users/admin.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
-from .models import CustomUser
+from .models import CustomUser, School
 
-
-from users.models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'nickname', 'school', 'grade', 'classroom')}),
+        (_('Personal info'),
+         {'fields': ('first_name', 'last_name', 'email', 'nickname', 'school', 'grade', 'classroom')}),
         (_('Permissions'), {
             'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         }),
@@ -20,9 +19,21 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'nickname', 'school', 'grade', 'classroom', 'is_active', 'is_staff'),
+            'fields': (
+                'username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'nickname', 'school', 'grade',
+                'classroom', 'is_active', 'is_staff'),
         }),
     )
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'nickname', 'school', 'grade', 'classroom')
+    list_display = (
+        'username', 'email', 'first_name', 'last_name', 'is_staff', 'nickname', 'school', 'grade', 'classroom')
     search_fields = ('username', 'first_name', 'last_name', 'email', 'nickname')
     ordering = ('username',)
+
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')  # Columns to display in the admin list view
+    search_fields = ['name']  # Fields to be searchable in the admin
+    list_filter = ('name',)  # Fields to filter in the admin sidebar
+
+# Register the School model with the customized options
--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from rest_framework import generics
 from .models import CustomUser
 from .serializers import UserRegistrationSerializer
 
+
 class UserRegistrationAPIView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserRegistrationSerializer
