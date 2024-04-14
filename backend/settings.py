4be9da000f94a898044a7c9729212e3a669493f1
hoetaek <hoetaek@student.snue.ac.kr>
2024-04-14 17:15:59 +0900

    feat: settings for test

--- backend/settings.py
+++ backend/settings.py
@@ -3,16 +3,11 @@ from pathlib import Path
 
 import environ
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
 env.read_env(env.str("ENV_PATH", ".env"))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env(
     "SECRET_KEY", default="insecure_2%0*g@d*)37cj%jpzx^o^239b&cynbgoz5$dd*j_zx*c36"
 )
@@ -50,6 +45,8 @@ REST_FRAMEWORK = {
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ),
 }
+
+USE_JWT = True
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
