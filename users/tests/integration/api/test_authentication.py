f0ad9edc8eae25b68465b73e97ea05a64b723107
hoetaek <hoetaek@student.snue.ac.kr>
2024-04-17 21:06:09 +0900

    fix: user info in test

--- users/tests/integration/api/test_authentication.py
+++ users/tests/integration/api/test_authentication.py
@@ -54,15 +54,15 @@ def test_학생은_닉네임만_입력해서_가입할_할_수_있다(client):
     )
 
     assert response.status_code == 201
-    assert response.data["username"] == "testuser"
-    assert response.data["nickname"] == "testnickname"
+    assert response.data["username"] == "studentuser"
+    assert response.data["nickname"] == "studentnickname"
     assert "access" in response.data
     assert "refresh" in response.data
 
-    assert User.objects.filter(username="testuser").exists()
-    test_user = User.objects.get(username="testuser")
+    assert User.objects.filter(username="studentuser").exists()
+    test_user = User.objects.get(username="studentuser")
 
-    assert test_user.nickname == "testnickname"
+    assert test_user.nickname == "studentnickname"
     assert test_user.is_active is True
     assert test_user.is_staff is False
 
