cbf094e41ca45e51831ddf66ae0ef87c26b3bb19
hoetaek <hoetaek@student.snue.ac.kr>
2024-04-17 20:34:12 +0900

    refactor: keep default user field stting

--- content/migrations/0002_initial.py
+++ content/migrations/0002_initial.py
@@ -10,9 +10,9 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ("content", "0001_initial"),
         ("contenttypes", "0002_remove_content_type_name"),
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
