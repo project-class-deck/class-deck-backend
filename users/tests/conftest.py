2d8dc2928dfb326faed3f5e004a82bda99b21e6e
hoetaek <hoetaek@student.snue.ac.kr>
2024-04-27 22:17:25 +0900

    test :user authentication api

--- users/tests/conftest.py
+++ users/tests/conftest.py
@@ -1,11 +1,10 @@
-import pytest
-from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import AccessToken
 
-User = get_user_model()
 
-
-@pytest.fixture
-def client():
-    api_client = APIClient()
-    return api_client
+def set_credentials(user):
+    token = AccessToken.for_user(user)
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f"Bearer {str(token)}")
+    client.user = user
+    return client
--- users/tests/integration/api/test_authentication.py
+++ users/tests/integration/api/test_authentication.py
@@ -1,151 +1,165 @@
 import pytest
+from dj_rest_auth.tests.mixins import APIClient
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 
+from users.tests.conftest import set_credentials
+from users.tests.factories.user_factory import UserFactory
+
 User = get_user_model()
 
 
-@pytest.fixture
-def user():
-    test_user = User.objects.create_user(
-        username="john", email="john@example.com", password="s3cr3t"
-    )
-    return test_user
-
-
 @pytest.mark.django_db
-def test_가입할_할_수_있다(client):
-    response = client.post(
-        reverse("user-register"),
-        {
-            "username": "testuser",
-            "email": "test@test.com",
-            "password": "testpassword1!",
-            "nickname": "testnickname",
-        },
-        format="json",
-    )
+class TestUserAuthentication:
 
-    assert response.status_code == 201
-    assert response.data["username"] == "testuser"
-    assert response.data["nickname"] == "testnickname"
-    assert "access" in response.data
-    assert "refresh" in response.data
+    @pytest.fixture
+    def user(self):
+        test_user = User.objects.create_user(
+            username="john", email="john@example.com", password="s3cr3t"
+        )
+        return test_user
 
-    assert User.objects.filter(username="testuser").exists()
-    test_user = User.objects.get(username="testuser")
+    @pytest.fixture
+    def no_login(self):
+        return APIClient()
 
-    assert test_user.nickname == "testnickname"
-    assert test_user.email == "test@test.com"
-    assert test_user.is_active is True
-    assert test_user.is_staff is False
+    def test_가입할_할_수_있다(self, no_login):
+        response = no_login.post(
+            reverse("user-register"),
+            {
+                "username": "testuser",
+                "email": "test@test.com",
+                "password": "testpassword1!",
+                "nickname": "testnickname",
+            },
+            format="json",
+        )
 
+        assert response.status_code == 201
+        assert response.data["username"] == "testuser"
+        assert response.data["nickname"] == "testnickname"
+        assert "access" in response.data
+        assert "refresh" in response.data
 
-@pytest.mark.django_db
-def test_학생은_닉네임만_입력해서_가입할_할_수_있다(client):
-    response = client.post(
-        reverse("student-register"),
-        {
-            "nickname": "studentnickname",
-        },
-        format="json",
-    )
+        assert User.objects.filter(username="testuser").exists()
+        test_user = User.objects.get(username="testuser")
 
-    assert response.status_code == 201
-    assert response.data["nickname"] == "studentnickname"
-    assert "access" in response.data
-    assert "refresh" in response.data
+        assert test_user.nickname == "testnickname"
+        assert test_user.email == "test@test.com"
+        assert test_user.is_active is True
+        assert test_user.is_staff is False
 
-    assert User.objects.filter(nickname="studentnickname").exists()
-    test_user = User.objects.get(nickname="studentnickname")
+    def test_학생은_닉네임만_입력해서_가입할_할_수_있다(self, no_login):
+        response = no_login.post(
+            reverse("student-register"),
+            {
+                "nickname": "studentnickname",
+            },
+            format="json",
+        )
 
-    assert test_user.is_active is True
-    assert test_user.is_staff is False
+        assert response.status_code == 201
+        assert response.data["nickname"] == "studentnickname"
+        assert "access" in response.data
+        assert "refresh" in response.data
 
+        assert User.objects.filter(nickname="studentnickname").exists()
+        test_user = User.objects.get(nickname="studentnickname")
 
-@pytest.mark.django_db
-def test_학생은_닉네임을_중복해서_가입할_할_수_있다(client):
-    response = client.post(
-        reverse("student-register"),
-        {
-            "nickname": "studentnickname",
-        },
-        format="json",
-    )
+        assert test_user.is_active is True
+        assert test_user.is_staff is False
 
-    assert response.status_code == 201
-    assert response.data["nickname"] == "studentnickname"
-    assert "access" in response.data
-    assert "refresh" in response.data
+    def test_학생은_닉네임을_중복해서_가입할_할_수_있다(self, no_login):
+        response = no_login.post(
+            reverse("student-register"),
+            {
+                "nickname": "studentnickname",
+            },
+            format="json",
+        )
 
-    assert User.objects.filter(nickname="studentnickname").exists()
+        assert response.status_code == 201
+        assert response.data["nickname"] == "studentnickname"
+        assert "access" in response.data
+        assert "refresh" in response.data
 
-    response = client.post(
-        reverse("student-register"),
-        {
-            "nickname": "studentnickname",
-        },
-        format="json",
-    )
+        assert User.objects.filter(nickname="studentnickname").exists()
 
-    assert response.status_code == 201
-    assert response.data["nickname"] == "studentnickname"
-    assert "access" in response.data
-    assert "refresh" in response.data
+        response = no_login.post(
+            reverse("student-register"),
+            {
+                "nickname": "studentnickname",
+            },
+            format="json",
+        )
 
-    assert User.objects.filter(nickname="studentnickname").count() == 2
+        assert response.status_code == 201
+        assert response.data["nickname"] == "studentnickname"
+        assert "access" in response.data
+        assert "refresh" in response.data
 
+        assert User.objects.filter(nickname="studentnickname").count() == 2
 
-@pytest.mark.django_db
-def test_사용자는_올바른_인증으로_로그인을_할_수_있다(client, user):
-    url = reverse("rest_login")
+    def test_사용자는_올바른_인증으로_로그인을_할_수_있다(self, user):
+        client = APIClient()
 
-    data = {"username": "john", "password": "s3cr3t"}
+        url = reverse("rest_login")
 
-    response = client.post(url, data)
+        data = {"username": "john", "password": "s3cr3t"}
 
-    assert response.status_code == status.HTTP_200_OK
-    assert "access" in response.data
-    assert "refresh" in response.data
+        response = client.post(url, data)
 
-    login_user = response.data["user"]
-    assert login_user["username"] == "john"
-    assert login_user["email"] == "john@example.com"
+        assert response.status_code == status.HTTP_200_OK
+        assert "access" in response.data
+        assert "refresh" in response.data
 
-    assert User.objects.filter(username="john").exists()
+        login_user = response.data["user"]
+        assert login_user["username"] == "john"
+        assert login_user["email"] == "john@example.com"
 
+        assert User.objects.filter(username="john").exists()
 
-@pytest.mark.django_db
-def test_사용자는_올바르지_않은_인증으로는_로그인을_할_수_없다(client):
-    url = reverse("rest_login")
+    def test_사용자는_올바르지_않은_인증으로는_로그인을_할_수_없다(self, no_login):
+        url = reverse("rest_login")
 
-    data = {"username": "john", "password": "wrongpassword"}
+        data = {"username": "john", "password": "wrongpassword"}
 
-    response = client.post(url, data)
+        response = no_login.post(url, data)
 
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert "non_field_errors" in response.data
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert "non_field_errors" in response.data
 
+    def test_학생은_비밀번호_없이도_인증정보를_제공할_수_있다(self, no_login, user):
+        response = no_login.post(
+            reverse("student-register"),
+            {
+                "nickname": "studentnickname",
+            },
+            format="json",
+        )
 
-@pytest.mark.django_db
-def test_학생은_비밀번호_없이도_인증정보를_제공할_수_있다(client, user):
-    response = client.post(
-        reverse("student-register"),
-        {
-            "nickname": "studentnickname",
-        },
-        format="json",
-    )
+        access = response.data["access"]
 
-    access = response.data["access"]
+        response = no_login.get(
+            reverse("rest_user_details"), HTTP_AUTHORIZATION=f"Bearer {access}"
+        )
 
-    response = client.get(
-        reverse("rest_user_details"), HTTP_AUTHORIZATION=f"Bearer {access}"
-    )
+        assert response.status_code == status.HTTP_200_OK
 
-    assert response.status_code == status.HTTP_200_OK
+        assert response.data["nickname"] == "studentnickname"
+        assert "username" in response.data
+        assert "email" in response.data
 
-    assert response.data["nickname"] == "studentnickname"
-    assert "username" in response.data
-    assert "email" in response.data
+    def test_사용자는_자신의_정보를_요청할_수_있다(self):
+        user = UserFactory(username="john", nickname="john")
+
+        client = set_credentials(user)
+
+        url = reverse("rest_user_details")
+
+        response = client.get(url)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["username"] == "john"
+        assert response.data["nickname"] == "john"
