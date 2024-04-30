f5e11c310cdf50a8deca66d999abda261d2a33f4
hoetaek <hoetaek@student.snue.ac.kr>
2024-05-01 01:41:09 +0900

    fix: 다른 method에 대해서도 권한 허용하기

--- content/permissions.py
+++ content/permissions.py
@@ -5,11 +5,11 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        elif request.method == "POST":
-            return request.user and request.user.is_authenticated
-        return False
+        return request.user and (
+            request.user.is_authenticated or request.user.is_superuser
+        )
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.author == request.user
+        return obj.author == request.user or request.user.is_superuser
