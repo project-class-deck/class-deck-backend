763f4d82c1715a6f80938b4f847fcba7881383a0
hoetaek <hoetaek@student.snue.ac.kr>
2024-05-12 00:40:13 +0900

    Feat: 기본으로 페이지네이션 15개씩 가져오기

--- backend/settings.py
+++ backend/settings.py
@@ -127,6 +127,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 15,
 }
 
 JWT_AUTH_COOKIE = "myhim-jwt"
