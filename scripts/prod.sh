554493e4630ded62177cfc60083be4fe1f5ec5d3
hoetaek <hoetaek@student.snue.ac.kr>
2024-05-12 18:28:14 +0900

    gunicorn run as daemon

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,1 +1,1 @@
-gunicorn backend.wsgi -w 3 -b 0.0.0.0:8000 --error-logfile /home/ec2-user/logs/error.log --access-logfile /home/ec2-user/logs/access.log
+gunicorn backend.wsgi --daemon -w 3 -b 0.0.0.0:8000 --error-logfile /home/ec2-user/logs/error.log --access-logfile /home/ec2-user/logs/access.log
