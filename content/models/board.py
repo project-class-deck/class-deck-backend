7fe10a4e505d8667cee36cf5e1fe13906eefbba8
hoetaek <hoetaek@student.snue.ac.kr>
2024-05-26 22:42:44 +0900

    create is_displayed field for board

--- content/models/board.py
+++ content/models/board.py
@@ -10,6 +10,7 @@ class Board(Likeable, models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="boards")
+    is_displayed = models.BooleanField(default=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
